20b58f0b7191b69746bfb9344baa60aff70b2fd9
Omar Amer <omar4444yasser@gmail.com>
2026-04-14 11:08:06 +0200

    did exercise 2

--- main.cpp
+++ main.cpp
@@ -1,23 +1,74 @@
 #include <iostream>
+#include <map>
 #include <string>
-#include <stdexcept>
-
 #include "utils.hpp"
 
 using namespace std;
 
 int main() {
-    string email;
+    map<string, string> users;
+    bool logged_in = false;
 
-        cout << "Enter your email address: ";
-        cin >> email;
+    while (true) {
+        int choice;
 
-        try {
-            check_email(email);
-            cout << "Valid email entered.\n";
-        } catch (runtime_error& e) {
-            cout << e.what() << " enter a valid email\n";
+        if (!logged_in) {
+            cout << "\n1. Sign up\n";
+            cout << "2. Login\n";
+            cout << "3. Exit\n";
+            cout << "Enter choice: ";
+            cin >> choice;
+
+            if (choice == 1) {
+                string email, password;
+
+                cout << "Enter email: ";
+                cin >> email;
+
+                try {
+                    check_email(email);
+                    cout << "Enter password: ";
+                    cin >> password;
+
+                    users[email] = password;
+                    cout << "Sign up successful.\n";
+                } catch (runtime_error& e) {
+                    cout << e.what() << endl;
+                }
+            }
+            else if (choice == 2) {
+                string email, password;
+
+                cout << "Enter email: ";
+                cin >> email;
+                cout << "Enter password: ";
+                cin >> password;
+
+                if (users[email] == password) {
+                    logged_in = true;
+                    cout << "Login successful.\n";
+                } else {
+                    cout << "Invalid email or password.\n";
+                }
+            }
+            else if (choice == 3) {
+                break;
+            }
+        } else {
+            cout << "\n1. Logout\n";
+            cout << "2. Exit\n";
+            cout << "Enter choice: ";
+            cin >> choice;
+
+            if (choice == 1) {
+                logged_in = false;
+                cout << "Logged out.\n";
+            }
+            else if (choice == 2) {
+                break;
+            }
         }
+    }
 
     return 0;
 }
